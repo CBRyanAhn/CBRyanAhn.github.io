453a8be858c7ef497d18b0e30c9cbaf8d2cee3a4
Changbum Ryan Ahn <28790837+CBRyanAhn@users.noreply.github.com>
2024-03-10 11:44:49 +0900

    Update load_contents.js

--- load_contents.js
+++ load_contents.js
@@ -6,11 +6,10 @@ $.ajaxSetup ({
 
 $(function(){
     $("#includedContentTop").load("top.html", 'f' + (Math.random()*1000000));;
+    $("#includedContentPub").load("currentservice.html", 'f' + (Math.random()*1000000));
     $("#includedContentPub").load("pub.html", 'f' + (Math.random()*1000000));
     $("#includedContentServices").load("services.html", 'f' + (Math.random()*1000000));
     $("#includedContentAwards").load("awards.html", 'f' + (Math.random()*1000000));
-    $("#includedContentTalks").load("talks.html", 'f' + (Math.random()*1000000));
-    $("#includedContentPatent").load("patent.html", 'f' + (Math.random()*1000000));
     $("#includedContentTeaching").load("teaching.html", 'f' + (Math.random()*1000000));
     $("#includedContentAdvising").load("advising.html", 'f' + (Math.random()*1000000));
     $("#includedContentFooter").load("footer.html", 'f' + (Math.random()*1000000));
