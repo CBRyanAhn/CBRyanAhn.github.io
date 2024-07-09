3f12e74ddfae7bbbb8ca447a69cc857b1cdba5f9
cylee1024 <ryan.cbahn@gmail.com>
2024-07-09 11:36:51 +0900

    Fixed workflow

--- .github/actions/checkout/checkout.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-echo "Checking out the repository..."
-git checkout $GITHUB_REF
--- .github/actions/setup-node/setup-node.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-echo "Setting up Node.js..."
-apk add --no-cache nodejs npm
-node -v
-npm -v
